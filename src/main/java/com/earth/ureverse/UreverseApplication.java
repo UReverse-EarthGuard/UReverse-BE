9fd428c6ef0a59871cd39981a99b800fda5a4c6c
BK <kbomin328@gmail.com>
2025-06-05 17:10:21 +0900

    init

---
+++ src/main/java/com/earth/ureverse/UreverseApplication.java
@@ -0,0 +1,13 @@
+package com.earth.ureverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UreverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UreverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/earth/ureverse/UreverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earth.ureverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UreverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
