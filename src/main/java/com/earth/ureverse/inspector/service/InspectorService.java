953fb39df549cd2564147e691955dc71b1f22150
BK <kbomin328@gmail.com>
2025-06-13 02:17:57 +0900

    feat: 검수 완료된 상품 상세 조회

--- src/main/java/com/earth/ureverse/inspector/service/InspectorService.java
+++ src/main/java/com/earth/ureverse/inspector/service/InspectorService.java
@@ -1,5 +1,6 @@
 package com.earth.ureverse.inspector.service;
 
+import com.earth.ureverse.inspector.dto.response.ProductInspectedDetailDto;
 import com.earth.ureverse.inspector.dto.response.ProductInspectionDetailDto;
 import com.earth.ureverse.inspector.dto.request.ProductSearchRequestDto;
 import com.earth.ureverse.inspector.dto.response.ProductSearchResultDto;
@@ -11,4 +12,6 @@ public interface InspectorService {
     ProductInspectionDetailDto getPendingProductDetail(Long productId);
 
     List<ProductSearchResultDto> searchProducts(Long userId, ProductSearchRequestDto requestDto);
+
+    ProductInspectedDetailDto getInspectedProductDetail(Long productId);
 }
