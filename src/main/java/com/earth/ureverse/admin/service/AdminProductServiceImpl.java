bd1a09bcd18cb51f935f6cd73b2a67b414ffbcf8
taeyeon0319 <xodus1349@likelion.org>
2025-06-12 23:05:36 +0900

    Refactor: 상품없을 경우 예외처리

--- src/main/java/com/earth/ureverse/admin/service/AdminProductServiceImpl.java
+++ src/main/java/com/earth/ureverse/admin/service/AdminProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.earth.ureverse.admin.service;
 import com.earth.ureverse.admin.dto.response.FinishProductResponse;
 import com.earth.ureverse.admin.dto.response.PickupProductResponse;
 import com.earth.ureverse.admin.dto.response.ProductInspectionResultResponse;
+import com.earth.ureverse.global.common.exception.NotFoundException;
 import com.earth.ureverse.global.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,10 +29,11 @@ public class AdminProductServiceImpl implements AdminProductService {
     @Override
     public ProductInspectionResultResponse getFinishProductDetail(Long productId) {
         ProductInspectionResultResponse result = productMapper.getFinishProductDetail(productId);
-        List<String> images = productMapper.getProductImages(productId);
-        if (result != null && result.getProduct() != null) {
-            result.getProduct().setImages(images);
+        if(result == null || result.getProduct() ==null){
+            throw new NotFoundException("해당 productId의 상품을 찾을 수 없습니다.");
         }
+        List<String> images = productMapper.getProductImages(productId);
+        result.getProduct().setImages(images);
         return result;
     }
 }
