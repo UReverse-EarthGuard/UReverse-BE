8ae994ed11d51328bde90282198c9baf8be4daa9
imyxxnsj <imyoonsj@gmail.com>
2025-06-16 15:33:53 +0900

    Refactor: 유효성 중복 검사 제거

--- src/main/java/com/earth/ureverse/global/auth/service/AuthService.java
+++ src/main/java/com/earth/ureverse/global/auth/service/AuthService.java
@@ -5,7 +5,6 @@ import com.earth.ureverse.global.auth.dto.request.LoginRequestDto;
 import com.earth.ureverse.global.auth.dto.request.RecoveryPasswordRequestDto;
 import com.earth.ureverse.global.auth.dto.request.SignUpRequestDto;
 import com.earth.ureverse.global.auth.dto.response.LoginResponseDto;
-import jakarta.validation.Valid;
 
 public interface AuthService {
 
@@ -17,6 +16,6 @@ public interface AuthService {
 
     String getUserName(CustomUserDetails customUserDetails);
 
-    void recoverPassword(@Valid RecoveryPasswordRequestDto recoveryPasswordRequestDto);
+    void recoverPassword(RecoveryPasswordRequestDto recoveryPasswordRequestDto);
 
 }
