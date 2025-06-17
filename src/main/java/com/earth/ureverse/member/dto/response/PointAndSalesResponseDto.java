4fb6dd80287b5a82c7cb622224355742c0748cc5
imyxxnsj <imyoonsj@gmail.com>
2025-06-17 22:41:39 +0900

    Feat: 포인트 내역 조회 API 응답 필드 추가

---
+++ src/main/java/com/earth/ureverse/member/dto/response/PointAndSalesResponseDto.java
@@ -0,0 +1,15 @@
+package com.earth.ureverse.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointAndSalesResponseDto {
+
+    private int totalPoint;
+    private int salesCount;
+
+}
