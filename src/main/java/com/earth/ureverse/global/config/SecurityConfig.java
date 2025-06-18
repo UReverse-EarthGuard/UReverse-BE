c590fb2985db2aa197395e03ceb8ead4676a35e0
pch8349 <chan449@naer.com>
2025-06-19 00:49:06 +0900

    fix: 로컬 통신용 작업. WebMvcConfigurer 구현

--- src/main/java/com/earth/ureverse/global/config/SecurityConfig.java
+++ src/main/java/com/earth/ureverse/global/config/SecurityConfig.java
@@ -17,13 +17,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
 
     private final JwtTokenProvider jwtTokenProvider;
 
